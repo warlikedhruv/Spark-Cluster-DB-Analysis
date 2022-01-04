9c64ee1ef90920cf7062f3a80da76d5caa6f2994
dhruvkumar <dhruvpatel719@yahoo.in>
2022-01-04 12:18:33 -0400

    Update spark_test.py
    test

--- spark_test.py
+++ spark_test.py
@@ -1,38 +1,55 @@
+# from pyspark.sql import SparkSession
+# import os
+#
+#
+# def connect_to_sql(spark, jdbc_hostname, jdbc_port, database, data_table, username, password):
+#     jdbc_url = "jdbc:mysql://{0}:{1}/{2}".format(jdbc_hostname, jdbc_port, database)
+#
+#     connection_details = {
+#         "user": username,
+#         "password": password,
+#         "driver": "com.mysql.cj.jdbc.Driver",
+#     }
+#
+#     df = spark.read.jdbc(url=jdbc_url, table=data_table, properties=connection_details)
+#     return df
+#     # print("os path", os.getcwd())
+#     # mysql_path = os.getcwd() + "/mysql-connector-java-5.1.34.jar"
+#     # print("MY SQL PATh", mysql_path)
+#     # print("ABS PATH", os.path.abspath(os.getcwd()))
+#     # if os.path.exists(mysql_path):
+#     #     print("SQL PATH EXISTS")
+#     # else:
+#     #     print("SQL PATH DOES NOT EXISTS")
+#     # return None
+#
+#
+#
+# if __name__ == '__main__':
+#     mysql_path = os.getcwd() + "/mysql-connector-java-5.1.34.jar"
+#     spark = SparkSession \
+#         .builder \
+#         .appName('test') \
+#         .master('local[*]') \
+#         .enableHiveSupport() \
+#         .config("spark.driver.extraClassPath", mysql_path) \
+#         .getOrCreate()
+#
+#     df = connect_to_sql(spark, 'localhost', '3306', 'AMS', 'crew', 'root', 'password')
+
+
+import findspark
+findspark.init()
+
+import pyspark # only run after findspark.init()
 from pyspark.sql import SparkSession
-import os
+spark = SparkSession.builder.getOrCreate()
 
+dataframe_mysql = spark.read.format("jdbc").options(
+    url="jdbc:mysql://localhost:3306/AMS",
+    driver = "com.mysql.jdbc.Driver",
+    dbtable = "company",
+    user="root",
+    password="password").load()
 
-def connect_to_sql(spark, jdbc_hostname, jdbc_port, database, data_table, username, password):
-    jdbc_url = "jdbc:mysql://{0}:{1}/{2}".format(jdbc_hostname, jdbc_port, database)
-
-    connection_details = {
-        "user": username,
-        "password": password,
-        "driver": "com.mysql.cj.jdbc.Driver",
-    }
-
-    df = spark.read.jdbc(url=jdbc_url, table=data_table, properties=connection_details)
-    return df
-    # print("os path", os.getcwd())
-    # mysql_path = os.getcwd() + "/mysql-connector-java-5.1.34.jar"
-    # print("MY SQL PATh", mysql_path)
-    # print("ABS PATH", os.path.abspath(os.getcwd()))
-    # if os.path.exists(mysql_path):
-    #     print("SQL PATH EXISTS")
-    # else:
-    #     print("SQL PATH DOES NOT EXISTS")
-    # return None
-
-
-
-if __name__ == '__main__':
-    mysql_path = os.getcwd() + "/mysql-connector-java-5.1.34.jar"
-    spark = SparkSession \
-        .builder \
-        .appName('test') \
-        .master('local[*]') \
-        .enableHiveSupport() \
-        .config("spark.driver.extraClassPath", mysql_path) \
-        .getOrCreate()
-
-    df = connect_to_sql(spark, 'localhost', '3306', 'AMS', 'crew', 'root', 'password')
+dataframe_mysql.show()
