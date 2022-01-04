5f522a77683892e4d0d51c58a746e0d5fe57f21b
dhruvkumar <dhruvpatel719@yahoo.in>
2022-01-04 12:01:40 -0400

    Update spark_test.py
    bug fix test

--- spark_test.py
+++ spark_test.py
@@ -1,27 +1,33 @@
 from pyspark.sql import SparkSession
 import os
-def connect_to_sql(
-    spark, jdbc_hostname, jdbc_port, database, data_table, username, password
-):
+
+
+def connect_to_sql(spark, jdbc_hostname, jdbc_port, database, data_table, username, password):
     jdbc_url = "jdbc:mysql://{0}:{1}/{2}".format(jdbc_hostname, jdbc_port, database)
 
-    connection_details = {
-        "user": username,
-        "password": password,
-        "driver": "com.mysql.jdbc.Driver",
-    }
+    # connection_details = {
+    #     "user": username,
+    #     "password": password,
+    #     "driver": "com.mysql.jdbc.Driver",
+    # }
+    #
+    # df = spark.read.jdbc(url=jdbc_url, table=data_table, properties=connection_details)
+    # return df
+    print("os path", os.getcwd())
+    mysql_path = os.getcwd() + "/mysql-connector-java-5.1.39"
+    print("MY SQL PATh", mysql_path)
+    return None
 
-    df = spark.read.jdbc(url=jdbc_url, table=data_table, properties=connection_details)
-    return df
 
-if __name__=='__main__':
-    mysql_path = os.getcwd()+ "/mysql-connector-java-5.1.39"
+
+if __name__ == '__main__':
+    mysql_path = os.getcwd() + "/mysql-connector-java-5.1.39"
     spark = SparkSession \
         .builder \
         .appName('test') \
         .master('local[*]') \
         .enableHiveSupport() \
-        .config("spark.driver.extraClassPath", mysql_path ) \
+        .config("spark.driver.extraClassPath", mysql_path) \
         .getOrCreate()
 
     df = connect_to_sql(spark, 'localhost', '3306', 'AMS', 'crew', 'root', 'password')
