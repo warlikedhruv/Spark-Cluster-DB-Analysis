a6169b479aca7f3db7b0073c2acd3cd6ae8b6bda
dhruvkumar <dhruvpatel719@yahoo.in>
2022-01-04 12:12:31 -0400

    Update spark_test.py
    bug fix test

--- spark_test.py
+++ spark_test.py
@@ -5,28 +5,28 @@ import os
 def connect_to_sql(spark, jdbc_hostname, jdbc_port, database, data_table, username, password):
     jdbc_url = "jdbc:mysql://{0}:{1}/{2}".format(jdbc_hostname, jdbc_port, database)
 
-    # connection_details = {
-    #     "user": username,
-    #     "password": password,
-    #     "driver": "com.mysql.jdbc.Driver",
-    # }
-    #
-    # df = spark.read.jdbc(url=jdbc_url, table=data_table, properties=connection_details)
-    # return df
-    print("os path", os.getcwd())
-    mysql_path = os.getcwd() + "/mysql-connector-java-5.1.34.jar"
-    print("MY SQL PATh", mysql_path)
-    print("ABS PATH", os.path.abspath(os.getcwd()))
-    if os.path.exists(mysql_path):
-        print("SQL PATH EXISTS")
-    else:
-        print("SQL PATH DOES NOT EXISTS")
-    return None
+    connection_details = {
+        "user": username,
+        "password": password,
+        "driver": "com.mysql.jdbc.Driver",
+    }
+
+    df = spark.read.jdbc(url=jdbc_url, table=data_table, properties=connection_details)
+    return df
+    # print("os path", os.getcwd())
+    # mysql_path = os.getcwd() + "/mysql-connector-java-5.1.34.jar"
+    # print("MY SQL PATh", mysql_path)
+    # print("ABS PATH", os.path.abspath(os.getcwd()))
+    # if os.path.exists(mysql_path):
+    #     print("SQL PATH EXISTS")
+    # else:
+    #     print("SQL PATH DOES NOT EXISTS")
+    # return None
 
 
 
 if __name__ == '__main__':
-    mysql_path = os.getcwd() + "/mysql-connector-java-5.1.39.jar"
+    mysql_path = os.getcwd() + "/mysql-connector-java-5.1.34.jar"
     spark = SparkSession \
         .builder \
         .appName('test') \
